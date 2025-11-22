863dcab7c5d12ad70a8cf2e0181c25c008293701
j6fgfhzwf2-ship-it <j6fgfhzwf2@privaterelay.appleid.com>
2025-11-22 11:11:56 +0100

    Add LiquidGradient view with animated gradient effect

--- swiftui-liquid-gradient/ContentView.swift
+++ swiftui-liquid-gradient/ContentView.swift
@@ -1,1 +1,22 @@
+import SwiftUI
+
+struct LiquidGradient: View {
+    @State private var move = false
+
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.blue, .purple, .pink]),
+                startPoint: move ? .topLeading : .bottomTrailing,
+                endPoint: move ? .bottomTrailing : .topLeading
+            )
+            .blur(radius: 80)
+            .animation(
+                .easeInOut(duration: 5).repeatForever(autoreverses: true),
+                value: move
+            )
+        }
+        .onAppear { move = true }
+    }
+}
 
